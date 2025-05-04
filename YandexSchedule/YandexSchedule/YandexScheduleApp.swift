bdea48ef34b9ebb2b110f4547cc21ea112a5b901
Daniil Oreshenkov <danieloreshenkov@gmail.com>
2025-05-04 09:09:13 +0300

    feat: Создан проект

---
+++ YandexSchedule/YandexSchedule/YandexScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexScheduleApp.swift
+//  YandexSchedule
+//
+//  Created by Daniil Oreshenkov on 04.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct YandexScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
