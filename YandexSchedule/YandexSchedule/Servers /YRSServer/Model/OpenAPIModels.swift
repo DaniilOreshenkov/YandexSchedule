ad7055cb929914f4d96f429cdc72ad0178104479
Daniil Oreshenkov <danieloreshenkov@gmail.com>
2025-05-05 09:43:14 +0300

    style: улучшите читабельность

--- YandexSchedule/YandexSchedule/Servers /YRSServer/Model/OpenAPIModels.swift
+++ YandexSchedule/YandexSchedule/Servers /YRSServer/Model/OpenAPIModels.swift
@@ -1,11 +1,18 @@
 import Foundation
 import OpenAPIRuntime
 
-typealias NearestStations = Components.Schemas.Stations
-typealias Schedule = Components.Schemas.Schedule
-typealias NearestCity = Components.Schemas.NearestCity
-typealias Copyright = Components.Schemas.Copyright
-typealias FlightSchedules = Components.Schemas.FlightSchedules
+// MARK: - Transport Entities
 typealias Thread = Components.Schemas.Thread
 typealias Carrier = Components.Schemas.Carrier
+
+// MARK: - Schedules and Routes
+typealias FlightSchedules = Components.Schemas.FlightSchedules
+typealias Schedule = Components.Schemas.Schedule
+
+// MARK: - Locations
+typealias NearestStations = Components.Schemas.Stations
+typealias NearestCity = Components.Schemas.NearestCity
 typealias StationsList = HTTPBody
+
+// MARK: - Misc
+typealias Copyright = Components.Schemas.Copyright
